d99133492a9dacfceabecf5b4f3bbd2ad0ddc0f2
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2020-01-24 13:55:09 +0100

    initial commit

---
+++ src/main/java/pl/bykowsi/kurs/tydzien1pd/Tydzien1pdApplication.java
@@ -0,0 +1,13 @@
+package pl.bykowsi.kurs.tydzien1pd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tydzien1pdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tydzien1pdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bykowsi/kurs/tydzien1pd/Tydzien1pdApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bykowsi.kurs.tydzien1pd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tydzien1pdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
