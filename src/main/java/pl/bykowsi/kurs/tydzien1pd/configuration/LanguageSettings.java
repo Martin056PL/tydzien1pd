08fb4bc2e8d55cefe7ab454ac039407173b4ffb9
Kamil Wawer <kamil.wawer@pollub.edu.pl>
2020-01-25 15:39:00 +0100

    Replace @Data for @Getter and change pole to final

--- src/main/java/pl/bykowsi/kurs/tydzien1pd/configuration/LanguageSettings.java
+++ src/main/java/pl/bykowsi/kurs/tydzien1pd/configuration/LanguageSettings.java
@@ -1,16 +1,16 @@
 package pl.bykowsi.kurs.tydzien1pd.configuration;
 
-import lombok.Data;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Data
+@Getter
 public class LanguageSettings {
 
-    private MessageSource messageSource;
+    private final MessageSource messageSource;
     private final String languageVersion;
 
     @Autowired
